e8a9bf157479087075266f84ed7838bee323767f
CNNKozmetik <cnnkozmetik@gmail.com>
2026-05-12 16:51:56 +0300

    2.0.0

---
+++ products.js
@@ -0,0 +1,120 @@
+const products = [
+
+  {
+    name: "Bambu Çubuklu Oda Kokusu",
+    image: "images/Kokular.jpg",
+    desc: "Kalıcı ve ferah ortam kokuları",
+    price: 190,
+    category: "oda-kokusu",
+    link: "https://www.instagram.com/canankozmetik.dorsy/"
+  },
+
+  {
+    name: "Dalbeyoğlu Esansiyel Yağlar",
+    image: "images/Esanslar.jpg",
+    desc: "Yoğun ve doğal esans çeşitleri",
+    price: 90,
+    category: "esansiyel",
+    link: "https://www.instagram.com/canankozmetik.dorsy/"
+  },
+
+  {
+    name: "Erkek Şampuan CrazyJhon 2In1",
+    image: "images/CJ_Man.jpg",
+    desc: "Erkek Şampuanı",
+    price: 150,
+    category: "sampuan",
+    link: "https://www.instagram.com/canankozmetik.dorsy/"
+  },
+
+  {
+    name: "Meyve Aromalı Şampuan CrazyJhon 2In1",
+    image: "images/CJ_Meyve.jpg",
+    desc: "Meyve Kokulu Şampuan",
+    price: 150,
+    category: "sampuan",
+    link: "https://www.instagram.com/canankozmetik.dorsy/"
+  },
+
+  {
+    name: "Arındırıcı Soyulabilir Siyah Maske - JEUNESSE",
+    image: "images/JEUN_Armask.png",
+    desc: "Ciltteki kir ve yağı temizler. Ölü cilt hücrelerinin uzaklaştırılmasına yardımcı olur.",
+    price: 150,
+    category: "maske",
+    link: "https://www.instagram.com/canankozmetik.dorsy/"
+  },
+
+  {
+    name: "Arındırıcı Soyulabilir Gümüş Maske - JEUNESSE",
+    image: "images/JEUN_Argumus.png",
+    desc: "Cilt tonunu eşitlemeye ve aydınlatmaya yardımcı olur. Ölü cilt hücrelerinin giderilmesine katkıda bulunur.",
+    price: 150,
+    category: "maske",
+    link: "https://www.instagram.com/canankozmetik.dorsy/"
+  },
+
+  {
+    name: "Kırışıklık Karşıtı Soyulabilir Altın Maske - JEUNESSE",
+    image: "images/JEUN_AltnMaske.png",
+    desc: "Cildinize esneklik, yumuşaklık ve sıkılık kazandırır.",
+    price: 150,
+    category: "maske",
+    link: "https://www.instagram.com/canankozmetik.dorsy/"
+  },
+
+  {
+    name: "Simli Soyulabilir Kolajen Maskesi - JEUNESSE",
+    image: "images/JEUN_Simlikolajen.png",
+    desc: "Cildinize esneklik, yumuşaklık ve sıkılık kazandırır.",
+    price: 150,
+    category: "maske",
+    link: "https://www.instagram.com/canankozmetik.dorsy/"
+  },
+
+  {
+    name: "Derinlemesine Peeling Etkili Kahve Peelingi - JEUNESSE",
+    image: "images/JEUN_Kahve.png",
+    desc: "Gözenekleri temizler ve arındırır. İçerdiği kafein cildi canlandırır ve pürüzsüz görünüm sağlar.",
+    price: 150,
+    category: "maske",
+    link: "https://www.instagram.com/canankozmetik.dorsy/"
+  },
+
+  {
+    name: "Detoks Etkili Kil Maskesi - JEUNESSE",
+    image: "images/JEUN_DetoxMaske.png",
+    desc: "Kil ve kremsi dokusu sayesinde ciltteki kiri ve pürüzleri temizler.",
+    price: 150,
+    category: "maske",
+    link: "https://www.instagram.com/canankozmetik.dorsy/"
+  },
+
+  {
+    name: "Pembe Kar Temizleyici Peeling Jeli - JEUNESSE",
+    image: "images/JEUN_Pembekar.png",
+    desc: "Tıkalı gözenekleri temizler ve cildi tazeler.",
+    price: 150,
+    category: "maske",
+    link: "https://www.instagram.com/canankozmetik.dorsy/"
+  },
+
+  {
+    name: "3'ü 1 Arada Maske - JEUNESSE",
+    image: "images/JEUN_3in1.png",
+    desc: "Yüz temizleyici, peeling ve maske etkisini bir arada sunar.",
+    price: 150,
+    category: "maske",
+    link: "https://www.instagram.com/canankozmetik.dorsy/"
+  },
+
+  {
+    name: "Işıltılı Soyulabilir Bal Maskesi - JEUNESSE",
+    image: "images/JEUN_Honeymask.jpg",
+    desc: "Bal özü sayesinde cildi temizler ve parlak görünüm kazandırır.",
+    price: 150,
+    category: "maske",
+    link: "https://www.instagram.com/canankozmetik.dorsy/"
+  }
+
+];
