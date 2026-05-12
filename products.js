b240afb8345b34637f0d0e6a8c946debb101f154
CNNKozmetik <cnnkozmetik@gmail.com>
2026-05-12 18:03:20 +0300

    2.1.0

--- products.js
+++ products.js
@@ -116,5 +116,392 @@ const products = [
     category: "maske",
     link: "https://www.instagram.com/canankozmetik.dorsy/"
   }
+  
+  {
+  name: "All Life Böğürtlen Kremi",
+  image: "images/alllife_bogurtlen_kremi.jpg",
+  desc: "Cildi nemlendirmeye yardımcı bakım kremi.",
+  price: 50,
+  category: "kremler",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "All Life Çilek Kremi",
+  image: "images/alllife_cilek_kremi.jpg",
+  desc: "Yumuşak ve ferah his bırakan bakım kremi.",
+  price: 50,
+  category: "kremler",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "At Kestanesi Balsamı",
+  image: "images/at_kestanesi_balsami.jpg",
+  desc: "Rahatlatıcı bakım balsamı.",
+  price: 190,
+  category: "kremler",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "C.J Şampuan & Duş Jeli Aurora",
+  image: "images/cj_sampuan_aurora.jpg",
+  desc: "Ferahlatıcı şampuan ve duş jeli.",
+  price: 150,
+  category: "sampuan",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "C.J Şampuan & Duş Jeli Erkek",
+  image: "images/cj_sampuan_erkek.jpg",
+  desc: "Erkeklere özel bakım şampuanı.",
+  price: 150,
+  category: "sampuan",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "C.J Meyve Özlü Şampuan",
+  image: "images/cj_meyve_ozlu.jpg",
+  desc: "Meyve aromalı bakım şampuanı.",
+  price: 150,
+  category: "sampuan",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Dalbeyoğlu Aktif Karbon Sabunu",
+  image: "images/dalbeyoglu_aktif_karbon.jpg",
+  desc: "Cildi arındırmaya yardımcı sabun.",
+  price: 115,
+  category: "sabun",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Dalbeyoğlu Kükürtlü Sabun",
+  image: "images/dalbeyoglu_kukurtlu.jpg",
+  desc: "Temizleyici bakım sabunu.",
+  price: 115,
+  category: "sabun",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Dalbeyoğlu Ayak Bakım Kremi",
+  image: "images/dalbeyoglu_ayak_bakim.jpg",
+  desc: "Ayak bakımına yardımcı krem.",
+  price: 190,
+  category: "kremler",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Dalbeyoğlu El ve Vücut Kremi",
+  image: "images/dalbeyoglu_el_vucut.jpg",
+  desc: "Yoğun nemlendirme sağlayan krem.",
+  price: 190,
+  category: "kremler",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "JEUNESSE Altın Maske",
+  image: "images/jeunesse_altin_maske.jpg",
+  desc: "Cilde canlı görünüm kazandıran maske.",
+  price: 150,
+  category: "maske",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "JEUNESSE Gümüş Maske",
+  image: "images/jeunesse_gumus_maske.jpg",
+  desc: "Cildi arındırmaya yardımcı bakım maskesi.",
+  price: 150,
+  category: "maske",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "JEUNESSE Siyah Maske",
+  image: "images/jeunesse_siyah_maske.jpg",
+  desc: "Gözenek temizliğine yardımcı maske.",
+  price: 150,
+  category: "maske",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "JEUNESSE Kahve Peeling",
+  image: "images/jeunesse_kahve_peeling.jpg",
+  desc: "Cildi canlandıran peeling etkisi.",
+  price: 150,
+  category: "maske",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "JEUNESSE Kolajen Maske",
+  image: "images/jeunesse_kolajen.jpg",
+  desc: "Cilde esneklik kazandırmaya yardımcı maske.",
+  price: 150,
+  category: "maske",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "LUZY ONE Erkek Parfüm Efendi",
+  image: "images/luzy_efendi.jpg",
+  desc: "Kalıcı erkek parfümü.",
+  price: 450,
+  category: "parfum",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "LUZY ONE Erkek Parfüm Legacy",
+  image: "images/luzy_legacy.jpg",
+  desc: "Yoğun ve etkileyici erkek parfümü.",
+  price: 450,
+  category: "parfum",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "LUZY ONE Kadın Parfüm Bella",
+  image: "images/luzy_bella.jpg",
+  desc: "Zarif kadın parfümü.",
+  price: 450,
+  category: "parfum",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "LUZY ONE Kadın Parfüm Flora",
+  image: "images/luzy_flora.jpg",
+  desc: "Çiçeksi notalara sahip kadın parfümü.",
+  price: 450,
+  category: "parfum",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Bambu Çubuklu Oda Kokusu",
+  image: "images/bambu_oda_kokusu.jpg",
+  desc: "Kalıcı ortam kokusu.",
+  price: 190,
+  category: "oda-kokusu",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Araç İçi Asma Koku",
+  image: "images/arac_ici_koku.jpg",
+  desc: "Araç içi ferahlatıcı koku.",
+  price: 90,
+  category: "arac-kokusu",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Dalbeyoğlu Esansiyel Yağ",
+  image: "images/dalbeyoglu_esansiyel.jpg",
+  desc: "Doğal aromatik esansiyel yağ.",
+  price: 90,
+  category: "esansiyel",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "LUZY ONE Erkek Parfüm Victory",
+  image: "images/luzy_victory.jpg",
+  desc: "Kalıcı ve ferah erkek parfümü.",
+  price: 450,
+  category: "parfum",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "LUZY ONE Erkek Parfüm Black",
+  image: "images/luzy_black.jpg",
+  desc: "Yoğun aromalı erkek parfümü.",
+  price: 450,
+  category: "parfum",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "LUZY ONE Kadın Parfüm Rose",
+  image: "images/luzy_rose.jpg",
+  desc: "Çiçeksi kadın parfümü.",
+  price: 450,
+  category: "parfum",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "LUZY ONE Kadın Parfüm Diamond",
+  image: "images/luzy_diamond.jpg",
+  desc: "Zarif ve kalıcı kadın parfümü.",
+  price: 450,
+  category: "parfum",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "LUZY ONE Kadın Parfüm Secret",
+  image: "images/luzy_secret.jpg",
+  desc: "Gün boyu etkili kadın parfümü.",
+  price: 450,
+  category: "parfum",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "JEUNESSE Detox Kil Maskesi",
+  image: "images/jeunesse_detox_kil.jpg",
+  desc: "Cildi temizlemeye yardımcı bakım maskesi.",
+  price: 150,
+  category: "maske",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "JEUNESSE Pembe Kar Peeling",
+  image: "images/jeunesse_pembe_kar.jpg",
+  desc: "Cildi yenileyen peeling jeli.",
+  price: 150,
+  category: "maske",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "JEUNESSE 3 in 1 Maske",
+  image: "images/jeunesse_3in1.jpg",
+  desc: "Temizleyici ve peeling etkili bakım maskesi.",
+  price: 150,
+  category: "maske",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "JEUNESSE Bal Maskesi",
+  image: "images/jeunesse_bal_maskesi.jpg",
+  desc: "Cilde parlak görünüm kazandıran bakım maskesi.",
+  price: 150,
+  category: "maske",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Dalbeyoğlu Lavanta Sabunu",
+  image: "images/dalbeyoglu_lavanta.jpg",
+  desc: "Lavanta özlü doğal bakım sabunu.",
+  price: 115,
+  category: "sabun",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Dalbeyoğlu Keçi Sütü Sabunu",
+  image: "images/dalbeyoglu_keci_sutu.jpg",
+  desc: "Keçi sütü içerikli bakım sabunu.",
+  price: 115,
+  category: "sabun",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Dalbeyoğlu Argan Yağlı Sabun",
+  image: "images/dalbeyoglu_argan.jpg",
+  desc: "Argan yağı destekli bakım sabunu.",
+  price: 115,
+  category: "sabun",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Dalbeyoğlu Gül Sabunu",
+  image: "images/dalbeyoglu_gul.jpg",
+  desc: "Gül özlü hoş kokulu bakım sabunu.",
+  price: 115,
+  category: "sabun",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Dalbeyoğlu Pirinç Sabunu",
+  image: "images/dalbeyoglu_pirinc.jpg",
+  desc: "Cilt bakımına yardımcı doğal sabun.",
+  price: 115,
+  category: "sabun",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Dalbeyoğlu Aloe Vera Sabunu",
+  image: "images/dalbeyoglu_aloe_vera.jpg",
+  desc: "Aloe vera destekli bakım sabunu.",
+  price: 115,
+  category: "sabun",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Dalbeyoğlu Çay Ağacı Sabunu",
+  image: "images/dalbeyoglu_cay_agaci.jpg",
+  desc: "Çay ağacı özlü temizleyici sabun.",
+  price: 115,
+  category: "sabun",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Dalbeyoğlu Ardıç Katranı Sabunu",
+  image: "images/dalbeyoglu_ardic.jpg",
+  desc: "Doğal içerikli bakım sabunu.",
+  price: 115,
+  category: "sabun",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Dalbeyoğlu Kükürt Kremi",
+  image: "images/dalbeyoglu_kukurt_krem.jpg",
+  desc: "Cilt bakımına yardımcı krem.",
+  price: 190,
+  category: "kremler",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Dalbeyoğlu Aynısefa Kremi",
+  image: "images/dalbeyoglu_aynisefa.jpg",
+  desc: "Bitkisel destekli bakım kremi.",
+  price: 190,
+  category: "kremler",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Dalbeyoğlu Kantaron Kremi",
+  image: "images/dalbeyoglu_kantaron.jpg",
+  desc: "Kantaron özlü bakım kremi.",
+  price: 190,
+  category: "kremler",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+},
+
+{
+  name: "Dalbeyoğlu Vazelin",
+  image: "images/dalbeyoglu_vazelin.jpg",
+  desc: "Nemlendirici bakım ürünü.",
+  price: 90,
+  category: "kremler",
+  link: "https://www.instagram.com/canankozmetik.dorsy/"
+}  
 
 ];
